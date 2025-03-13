421370a8987a5de32d308dd0e5cf62b5490a1dee
조동희(백엔드개발1팀) <jodonghee@hanssem.com>
2025-03-13 13:51:51 +0900

    first commit

---
+++ api/src/main/java/com/project/api/adapter/in/WebController.java
@@ -0,0 +1,17 @@
+package com.project.api.adapter.in;
+
+import com.project.core.StringUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/delivery/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
+public class WebController {
+
+  @GetMapping
+  void abc() {
+    StringUtils.print("asd");
+  }
+}
---
+++ core/src/main/java/com/project/core/StringUtils.java
@@ -0,0 +1,12 @@
+package com.project.core;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StringUtils {
+
+  public static void print(String str) {
+    log.info("str :: {}", str);
+  }
+}
